397461b12932920fb0bf8d7a3fe096b19aeb7f1b
bencroskery <ben@irtech.com>
2016-06-27 21:37:12 -0400

    :laughing:
    Initial Bundle!

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,10 +1,1 @@
-module.exports = {
-  maximumFileSizeToCacheInBytes: 20000000,
-  staticFileGlobs: [
-    '/index.html',
-    '/manifest.json',
-    '/src/list.json',
-    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
-  ],
-  navigateFallback: '/index.html'
-};
+module.exports={maximumFileSizeToCacheInBytes:2e7,staticFileGlobs:["/index.html","/manifest.json","/src/list.json","/bower_components/webcomponentsjs/webcomponents-lite.min.js"],navigateFallback:"/index.html"};
