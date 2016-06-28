781d045045857aa746689ad60cacd3ba3c6d8a95
bencroskery <ben@irtech.com>
2016-06-27 22:44:42 -0400

    :pig_nose:
    Links for service worker were bad.

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,10 +1,11 @@
 module.exports = {
+  replacePrefix: '/emoji-list',
   maximumFileSizeToCacheInBytes: 20000000,
   staticFileGlobs: [
-    'index.html',
-    'manifest.json',
-    'src/list.json',
-    'bower_components/webcomponentsjs/webcomponents-lite.min.js'
+    '/index.html',
+    '/manifest.json',
+    '/src/list.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
   ],
-  navigateFallback: 'index.html'
+  navigateFallback: '/index.html'
 };
