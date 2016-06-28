7f836334b93f6df99836bfaf154779096d3bf91f
bencroskery <ben@irtech.com>
2016-06-27 21:29:15 -0400

    :grinning:
    Initial Commit!

---
+++ sw-precache-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  maximumFileSizeToCacheInBytes: 20000000,
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/src/list.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js'
+  ],
+  navigateFallback: '/index.html'
+};
